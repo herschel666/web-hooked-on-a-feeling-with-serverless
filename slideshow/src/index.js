0ef5bcb669314ded64a078cd91a9125d469acc65
Emanuel Kluge <emanuel.kluge@jimdo.com>
2018-04-23 08:30:23 +0200

    chore: adjustments

--- slideshow/src/index.js
+++ slideshow/src/index.js
@@ -3,12 +3,6 @@ import fa from '@fortawesome/fontawesome';
 import brands from '@fortawesome/fontawesome-free-brands';
 import { initStaticTwitterWidget } from './widget/widget';
 
-const doTheTwister = () =>
-  initStaticTwitterWidget(
-    'https://d2r0rsl0n2e9pc.cloudfront.net/',
-    'static-twitter-widget'
-  ).then(() => setTimeout(doTheTwister, 1000 * 5));
-
 const main = () => {
   fa.dom.i2svg({
     node: document.querySelector('.slides'),
@@ -24,7 +18,10 @@ const main = () => {
     history: true,
   });
 
-  doTheTwister();
+  initStaticTwitterWidget(
+    'https://d2r0rsl0n2e9pc.cloudfront.net/',
+    'static-twitter-widget'
+  )();
 };
 
 document.addEventListener('DOMContentLoaded', main);
